b04be7c394abaa7b32b1c14389d74ee990768210
DeoEsor <RulianValeriy@yandex.ru>
2022-04-28 13:29:59 +0300

    Email Validator Done

---
+++ EmailValidator/Services/EmailValidatorRequestReciver.cs
@@ -0,0 +1,53 @@
+﻿using Grpc.Net.Client;
+using RabbitMQReceiver.Interfaces;
+using Validation.Mediator;
+// ReSharper disable PrivateFieldCanBeConvertedToLocalVariable
+
+namespace EmailValidator.Services;
+
+//TODO : Refactor RequestReceivers to Generic
+public class AddressValidatorRequestReceiver
+{
+    private readonly ILogger<AddressValidatorRequestReceiver> _logger;
+
+    private readonly Validation.EmailValidator.EmailValidatorClient _client;
+
+    private readonly IMQRpcReceiver<EmailValidationRequests, EmailValidationReplies> _mqRpcReceiver;
+
+    public AddressValidatorRequestReceiver(ILogger<AddressValidatorRequestReceiver> logger, 
+                                            IMQRpcReceiver<EmailValidationRequests, EmailValidationReplies> mqRpcReceiver)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _mqRpcReceiver = mqRpcReceiver ?? throw new ArgumentNullException(nameof(mqRpcReceiver));
+
+        var httpHandler = new HttpClientHandler();
+
+        httpHandler.ServerCertificateCustomValidationCallback =
+            
+            HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+        var channel = GrpcChannel.ForAddress("https://localhost:5001",
+            new GrpcChannelOptions
+            {
+                HttpHandler = httpHandler
+            });
+
+        _client = new Validation.EmailValidator.EmailValidatorClient(channel);
+        _mqRpcReceiver.RPC = ValidateAddressAsync;
+    }
+    
+    private async Task<EmailValidationReplies> ValidateAddressAsync(EmailValidationRequests request, CancellationToken token = default)
+    {
+        try
+        {
+            _logger.LogInformation($"Received to validate from Rabbit - {request}");
+            return await _client.ValidateAsync(request, new Grpc.Core.CallOptions(cancellationToken: token));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogCritical($" Exception : {ex.Message}");
+        }
+
+        return null!;
+    }
+}
---
+++ EmailValidator/Services/EmailValidatorService.cs
@@ -0,0 +1,57 @@
+﻿using System.Net.Mail;
+using Grpc.Core;
+using Validation.Mediator;
+
+namespace EmailValidator.Services;
+
+public class EmailValidatorService : Validation.EmailValidator.EmailValidatorBase
+{
+    private readonly ILogger<EmailValidatorService> _logger;
+
+    public EmailValidatorService(ILogger<EmailValidatorService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override async Task<EmailValidationReplies> Validate(EmailValidationRequests request, ServerCallContext context)
+    {
+        try
+        {
+            var reply = new EmailValidationReplies();
+            foreach (var email in request.Emails)
+                reply.Emails.Add(Validate(email));
+
+            return reply;
+        }
+        catch (Exception e)
+        {
+            _logger.LogCritical($"Exception {e.Message}");
+            throw;
+        }
+
+        return null!;
+    }
+
+    private EmailValidationReply Validate(EmailValidationRequest email)
+    {
+        var emailValidationResult = new StringValidationResult();
+        var trimmedEmail = email.Email.Trim();
+
+        if (trimmedEmail.EndsWith(".")) 
+            emailValidationResult.IsValid = false; // suggested by @TK-421
+        try
+        {
+            var addr = new MailAddress(email.Email);
+            emailValidationResult.IsValid = addr.Address == trimmedEmail;
+        }
+        catch (Exception exception)
+        {
+            emailValidationResult.IsValid = false;
+            emailValidationResult.Comment = exception.Message;
+        }
+        return new EmailValidationReply
+        {
+            Email = emailValidationResult
+        };
+    }
+}
